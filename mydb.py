a0703f4f0218b66d144666b657ac281a9b30776b
laizin <laizin2107@gmail.com>
2023-10-18 11:55:36 +0400

    initial

---
+++ mydb.py
@@ -0,0 +1,11 @@
+import mysql.connector
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'dev123!@#&*('
+)
+# Prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# One time executable
+cursorObject.execute("CREATE DATABASE crmdatabase") 
