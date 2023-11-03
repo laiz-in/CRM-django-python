6af713abfc334368d240d6636846dbb6d4b01a40
laizin <laizin2107@gmail.com>
2023-11-03 12:32:54 +0400

    search bar

--- webapp/static/js.js
+++ webapp/static/js.js
@@ -11,3 +11,41 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 1000); // Time in milliseconds - 3 seconds
     });
 });
+
+
+function searchFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.querySelector(".table-data");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0]; // Adjust index as per the column you want to search
+
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function searchNames(input) {
+    var names = document.querySelectorAll('tbody tr');
+
+    names.forEach(function(name) {
+        var fullName = name.getElementsByTagName('td')[0].innerText.toLowerCase(); // Name
+        var id = name.getElementsByTagName('td')[8].innerText.toLowerCase(); // ID
+
+        if (fullName.includes(input.toLowerCase()) || id.includes(input.toLowerCase())) {
+            name.style.display = "table-row"; // Show the row if either name or ID matches
+        } else {
+            name.style.display = "none"; // Hide the row if neither name nor ID matches
+        }
+    });
+}
--- webapp/views.py
+++ webapp/views.py
@@ -90,12 +90,13 @@ def add_record(request):
 def update_record(request, pk):
 	if request.user.is_authenticated:
 		current_record = Record.objects.get(id=pk)
+		customer_record = current_record
 		form = AddRecordForm(request.POST or None, instance=current_record)
 		if form.is_valid():
 			form.save()
 			messages.success(request, "Record Has Been Updated!")
 			return redirect('home')
-		return render(request, 'update_record.html', {'form':form})
+		return render(request, 'update_record.html', {'form': form, 'customer_record': customer_record})
 	else:
 		messages.success(request, "You Must Be Logged In...")
 		return redirect('home')
